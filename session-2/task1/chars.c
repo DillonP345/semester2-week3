a2b602a9e4d593498cf9e1858ba0a0071cb0f449
DillonP345 <cdnk0959@leeds.ac.uk>
2026-02-13 12:50:11 +0000

    end of session2

--- session-2/task1/chars.c
+++ session-2/task1/chars.c
@@ -7,8 +7,8 @@
 #include <stdio.h>
 
 int main( void ) {
-    char c = 'a';
-    int k = 95;
+    char c = 'b';
+    int k = 96;
 
     printf("Display char as char: %c\n",c);
     printf("Display char as int: %d\n",c);
@@ -16,4 +16,5 @@ int main( void ) {
     printf("Display int as char: %c\n",k);
 
     return 0;
-}
+} 
+
--- session-2/task1/sizes.c
+++ session-2/task1/sizes.c
@@ -11,10 +11,16 @@ int main( void ) {
     char testChar;
     int testInt;
     float testFloat;
+    short int testshortint;
+    long int testlongint;
+    double testdouble;
 
     printf("Char size is %ld bytes\n",sizeof(testChar));
     printf("Int size is %ld bytes\n",sizeof(testInt));
     printf("Float size is %ld bytes\n",sizeof(testFloat));
+    printf("long Int size is %ld bytes\n",sizeof(testlongint));
+    printf("short Int size is %ld bytes\n",sizeof(testshortint));
+    printf("double Int size is %ld bytes\n",sizeof(testdouble));
 
     return 0;
 }
