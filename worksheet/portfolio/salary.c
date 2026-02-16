9edeeb306708f6b25afe357663559633f44a9b87
DillonP345 <cdnk0959@leeds.ac.uk>
2026-02-16 13:47:17 +0000

    save a bit

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,21 +3,24 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Dillon Phillips
+ * ID: 201984193
  */
 
  int main( void ) {
 
     // define and initialise variables for the problem data 
-
+   float Salary;
+   float NI_contribution;
+   float Tax_contribution;
+   float Take_home;
     // calculate the deductions and final take-home salary
 
     // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+    printf("Salary £...", Salary);
+    printf("NI contribution £...",NI_contribution);
+    printf("Tax contribution £...",Tax_contribution);
+    printf("Take home salary £...",Take_home);
 
     return 0;
  }
