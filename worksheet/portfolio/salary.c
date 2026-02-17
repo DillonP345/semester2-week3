60d4fe179681a3fcd3b214f6a282ea9bc35e4a75
DillonP345 <cdnk0959@leeds.ac.uk>
2026-02-17 14:38:10 +0000

    save

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -10,23 +10,26 @@
  int main( void ) {
 
     // define and initialise variables for the problem data 
-   float Salary = 36500;
-   float NI = 0.08;
-   float Tax = 0.15 ;
+   float Salary = 30000;
+   float NI = 0.09;
+   float Tax = 0.25 ;
+   float Salary_remaining;
    float NI_contribution;
    float Tax_contribution;
    float Take_home;
     // calculate the deductions and final take-home salary
    NI_contribution = Salary * NI;
-   Salary = Salary - NI_contribution;
+   Salary_remaining = Salary - NI_contribution;
 
-   if (Salary>12500)
+   if (Salary_remaining>12500)
    {
-      Salary = Salary - 12500;
-      Tax_contribution = Salary * Tax;
-      Salary = Salary - Tax_contribution;
+      Tax_contribution = (Salary_remaining - 12500) * Tax;
    }
-   Take_home = Salary + 12500;
+   else
+   {
+      Tax_contribution = 0;
+   }
+   Take_home = Salary_remaining - Tax_contribution;
 
 
     // Use only these print statement with appropriate formatting and variable names
